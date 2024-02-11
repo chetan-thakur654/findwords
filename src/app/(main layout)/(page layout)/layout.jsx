cdd53057f4212aab841e777f6077550d6b551fd1
Chetan Rasik <chetanrasik7@gmail.com>
2024-02-11 23:15:03 +0530

    added ads.txt and adsense script  for revewing

--- src/app/(main layout)/(page layout)/layout.jsx
+++ src/app/(main layout)/(page layout)/layout.jsx
@@ -4,42 +4,50 @@ import WordFinderForm from "../../../../components/form/form";
 import { mainPages } from "../../../../data/mainPages";
 import { SlArrowRight } from "react-icons/sl";
 import Link from "next/link";
+import Script from "next/script";
 
 export default function layout({ children }) {
   return (
-    <div className={styles.home}>
-      <div className={styles.left}>{children}</div>
-      <aside className={styles.right}>
-        <div className="sidebar-Item">
-          <div className="filter-words">
-            <h2>Advanced Word Search</h2>
-            <WordFinderForm />
-          </div>
-        </div>
-        <hr />
-        <div className="sidebar-Item">
-          <div className={styles["other-words-finder"]}>
-            <h2>Other Words Finder</h2>
-            <div className="finder-list">
-              <ul>
-                {mainPages?.map(({ name, link, data }) => (
-                  <li>
-                    <Link href={`/${link}`}>
-                      <div>
-                        <h3>{name}</h3>
-                        <span>{data}</span>
-                      </div>
-                      <div>
-                        <SlArrowRight />
-                      </div>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
+    <>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6954377897433069"
+        crossorigin="anonymous"
+      ></Script>
+      <div className={styles.home}>
+        <div className={styles.left}>{children}</div>
+        <aside className={styles.right}>
+          <div className="sidebar-Item">
+            <div className="filter-words">
+              <h2>Advanced Word Search</h2>
+              <WordFinderForm />
             </div>
           </div>
-        </div>
-      </aside>
-    </div>
+          <hr />
+          <div className="sidebar-Item">
+            <div className={styles["other-words-finder"]}>
+              <h2>Other Words Finder</h2>
+              <div className="finder-list">
+                <ul>
+                  {mainPages?.map(({ name, link, data }) => (
+                    <li>
+                      <Link href={`/${link}`}>
+                        <div>
+                          <h3>{name}</h3>
+                          <span>{data}</span>
+                        </div>
+                        <div>
+                          <SlArrowRight />
+                        </div>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </aside>
+      </div>
+    </>
   );
 }
